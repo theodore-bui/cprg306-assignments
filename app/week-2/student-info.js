645700cced00fb134aabd53289ffd9a10fd32af4
Theodore Bui <theodore.bui@edu.sait.ca>
2024-01-16 13:14:47 -0700

    Update home page and add week 2 page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo(){
+    return (
+        <div>
+            <p>Theodore Bui</p>
+            <Link href="https://github.com/theodore-bui">https://github.com/theodore-bui</Link>
+        </div>
+    )
+}
